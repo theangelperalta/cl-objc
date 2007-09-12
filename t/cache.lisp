fd91631376027530ea46db7d91acf3344aec9d54
matley <matley@muppetslab.org>
2007-09-12 12:58:08 +0000

    Fix to cache testcase to avoid out of memory
    darcs-hash:c9e8b725b9124344cf09429704b0a44e0b068fd0

--- t/cache.lisp
+++ t/cache.lisp
@@ -2,31 +2,30 @@
 
 (in-suite :cache)
 
-(test cache-compile
-    (delete-clos-definitions)
-  
-  ;; construct a new cache and definitions
-  (let ((tmp-cache-filename (make-pathname :directory (pathname-directory (user-homedir-pathname))
-					   :name "tmp-cache-cl-objc"
-					   :type "lisp")))
-    (with-open-file (cache-stream tmp-cache-filename 
-				  :direction :output 
-				  :if-exists :supersede
-				  :if-does-not-exist :create)
-      (update-clos-definitions :output-stream cache-stream :force t)
-      (compile-file tmp-cache-filename))
+(defun tmp-cache-filename ()
+  (make-pathname :directory (pathname-directory (user-homedir-pathname))
+		 :name "tmp-cache-cl-objc"
+		 :type "lisp"))
 
-    ;; some tries
-    (is (make-instance (intern "NS-NUMBER" "OBJC")))
-    (is
-     (funcall (intern "INT-VALUE" "OBJC") 
-	      (funcall (intern "NUMBER-WITH-INT?" "OBJC")
-		       (meta (intern "NS-NUMBER" "OBJC"))
-		       5)))))
+(test cache-compile
+  ;; construct a new cache and definitions
+  (with-open-file (cache-stream (tmp-cache-filename) 
+				:direction :output 
+				:if-exists :supersede
+				:if-does-not-exist :create)
+    (update-clos-definitions :output-stream cache-stream :force t)
+    (compile-file (tmp-cache-filename)))
+
+  ;; some tries
+  (is (make-instance (intern "NS-NUMBER" "OBJC")))
+  (is
+   (funcall (intern "INT-VALUE" "OBJC") 
+	    (funcall (intern "NUMBER-WITH-INT?" "OBJC")
+		     (meta (intern "NS-NUMBER" "OBJC"))
+		     5))))
 
 (test cache-load 
-  (delete-clos-definitions)
-  (load (compile-file-pathname tmp-cache-filename))
+  (load (compile-file-pathname (tmp-cache-filename)))
   
   ;; some tries
   (is (make-instance (intern "NS-NUMBER" "OBJC")))
@@ -36,4 +35,4 @@
 		     (meta (intern "NS-NUMBER" "OBJC"))
 		     5)))
   ;; deleting temporary cache
-  (delete-file tmp-cache-filename))
+  (delete-file (tmp-cache-filename)))
